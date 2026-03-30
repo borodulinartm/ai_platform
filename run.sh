fd385f4fc0488e7d97fe886d271bdd090e6c5637
artem <a@a.ru>
2026-03-30 17:12:02 +0300

    Modify run script

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-pkill -9 'ai_platform*'
+PID_FILE=app.pid
+
+if [ -f $PID_FILE ]; then
+  PID=$(cat $PID_FILE)
+  echo "Stopping app with PID $PID"
+  kill $PID
+  sleep 5
+fi
 
 mvn clean install -Dmaven.test.skip=true
-nohup /opt/jdk21/bin/java -jar target/ai_platform-0.0.1-SNAPSHOT.jar > /dev/null &
+#nohup /opt/jdk21/bin/java -jar target/ai_platform-0.0.1-SNAPSHOT.jar > /dev/null &
+
+nohup java -jar target/ai_platform-0.0.1-SNAPSHOT.jar > /dev/null &
+echo $! > $PID_FILE
